aa4ae0ab45e006ffe5af3181a5bc04b2809a3177
Nazario Lupen <nazario.lupen@gmail.com>
2024-11-21 00:53:13 +0200

    commit name and surname

--- Main.py
+++ Main.py
@@ -2,3 +2,10 @@ a = 10
 b = 28
 c = a + b
 print("додавання а і b = ", c)
+
+name = "Nazario"
+surname = "Lupen"
+
+code = "il tuo nome è {} e il tuo cognome è {}".format(name,surname)
+
+print(code)
